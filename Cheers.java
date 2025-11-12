9c3bb9c3bde03bf4b68ec7f88f8be236b34fe35b
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2025-11-12 20:40:52 +0200

    update

--- Cheers.java
+++ Cheers.java
@@ -15,15 +15,15 @@ int times = Integer.parseInt(args[1]);
 for (int i = 0; i < name.length(); i++) {
         char current = name.charAt(i);
         if (anLetters.indexOf(current) == -1 ) {
-                System.out.println("Give me a: " + current +": " + current +"!");
+                System.out.println("Give me a " + current + ": " + current +"!");
         }
         else
-        System.out.println("Give me an: " + current +": " + current +"!");
+        System.out.println("Give me an " + current +": " + current +"!");
 }
         System.out.println("What does that spell?");
 
 for (int i = 0; i < times; i++) {
-        System.out.println(name + " !!!");
+        System.out.println(name + "!!!");
 }
 
 }
