172d728c5f1224fe4914bb26cb7a4cb97b7bf93a
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2025-11-12 20:36:50 +0200

    update

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,23 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+String name = args[0];
+name = name.toUpperCase(); // פונקציה של סטרינג שהופכת את כל המחרוזת לאותיות גדולות
+String anLetters = "AEFHILMNOR";
+int times = Integer.parseInt(args[1]);
+for (int i = 0; i < name.length(); i++) {
+        char current = name.charAt(i);
+        if (anLetters.indexOf(current) == -1 ) {
+                System.out.println("Give me a: " + current +": " + current +"!");
         }
+        else
+        System.out.println("Give me an: " + current +": " + current +"!");
+}
+        System.out.println("What does that spell?");
+
+for (int i = 0; i < times; i++) {
+        System.out.println(name + " !!!");
+}
+
+}
 }
