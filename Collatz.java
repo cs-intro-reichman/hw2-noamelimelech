57acd8711cabe9b827c497d1e41bda77eb272c5d
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2025-11-13 20:36:17 +0200

    upload Collatz

--- Collatz.java
+++ Collatz.java
@@ -2,26 +2,42 @@
 public class Collatz {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
+        String mode = args[1];  // "v" or "c"
 
         for (int i = 1; i <= number; i++) {
             int current = i;
             int max = i;
 
-            while (current != 1) {
-                System.out.print(current + " ");
-
-                if (current % 2 == 0) {
-                    current = current / 2;
-                } else {
-                    current = 3 * current + 1;
+            if (mode.equals("v")) {
+                // טיפול מיוחד למספר 1
+                if (i == 1) {
+                    System.out.println("1 4 2 1 (4)");
+                    continue;
                 }
-
-                if (current > max) {
-                    max = current;
+                while (current != 1) {
+                    System.out.print(current + " ");
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = 3 * current + 1;
+                    }
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+                System.out.println("1 (" + max + ")");
+            } 
+            else if (mode.equals("c")) {
+                while (current != 1) {
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = 3 * current + 1;
+                    }
                 }
             }
-            System.out.println("1 (" + max + ")");
         }
+
         System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
     }
 }
